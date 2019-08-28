3ba75b8cba1e49303b90aa9a27eca2061cc0648e
Bogdan Arsenic <bogdanarsenic@gmail.com>
2019-08-28 16:27:32 +0200

    Registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { CheckavailService } from './checkavail/checkavail.service';
 import {HttpClientModule} from '@angular/common/http';
 import { HttpClientXsrfModule } from '@angular/common/http';
 import { GuestComponent } from './guest/guest.component';
+import { AllUsersComponent } from './all-users/all-users.component';
 
 const appRoutes:Routes=[
   
@@ -36,6 +37,7 @@ const appRoutes:Routes=[
     HomeComponent,
     NavbarComponent,
     GuestComponent,
+    AllUsersComponent,
     
     
   ],
